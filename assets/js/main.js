e4493d1d6e7fadc057847a630fb716d441c03df1
futoricky <ricardoamercado@gmail.com>
2016-10-25 18:06:24 -0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.tlt').textillate({
+    loop: true,
+    autoStart: true,
+    in: {
+      effect: 'jello',
+      sync: true
+    },
+    out: {
+      effect: 'jello',
+      sync: true
+    }
+  });
+});
